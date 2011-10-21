28d2c9f4ef0227a356454eb9afac44f3ccbe52b3
faust45 <faust451@gmail.com>
2011-10-21 14:55:25 +0300

    Add task 2

---
+++ case_study/task_2.rb
@@ -0,0 +1,13 @@
+records = [
+  {:name => 'Alex',  :age => 10},
+  {:name => 'Mary',  :age => 25},
+  {:name => 'Steve', :age => 14},
+  {:name => 'Mila',  :age => 20}
+]
+
+
+def find_oldest_person
+end
+
+
+puts find_oldest_person
