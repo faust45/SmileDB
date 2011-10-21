b08c03ab3c14bb807e3467718d1b53e6dc435631
faust45 <faust451@gmail.com>
2011-10-21 14:33:34 +0300

    Add task 1

---
+++ case_study/task_1.rb
@@ -0,0 +1,13 @@
+records = [
+  {:name => 'Alex',  :tags => ['music', 'programming', 'air']},
+  {:name => 'Mary',  :tags => ['hack', 'film', 'pockert']},
+  {:name => 'Steve', :tags => ['air', 'dance', 'programming']},
+  {:name => 'Mila',  :tags => ['film', 'dance', 'fly']}
+]
+
+
+def find_with_tag(tag)
+end
+
+
+puts find_with_tag('film')
